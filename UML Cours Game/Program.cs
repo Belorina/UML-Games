6c6148ea82485e4c60abc6221af304ec285bcc0f
Belorina <ozcan_berrin@hotmail.com>
2021-05-27 09:42:23 +0200

    first commit

---
+++ UML Cours Game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UML_Cours_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
