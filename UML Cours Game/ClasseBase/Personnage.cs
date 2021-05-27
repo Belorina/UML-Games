08ed9bdaccf653f2de20447afc9a202311404a16
Belorina <ozcan_berrin@hotmail.com>
2021-05-27 10:35:36 +0200

    object creation in program

--- UML Cours Game/ClasseBase/Personnage.cs
+++ UML Cours Game/ClasseBase/Personnage.cs
@@ -40,6 +40,12 @@ namespace ClasseBase
             Console.WriteLine("J'attaque!");
         }
 
+
+        public string obtenirDescription ()
+        {
+            string description = "je suis un personnage hello";
+            return (description);
+        }
     }
 
 
--- UML Cours Game/Program.cs
+++ UML Cours Game/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ClasseBase;
 
 namespace UML_Cours_Game
 {
@@ -7,6 +8,23 @@ namespace UML_Cours_Game
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Personnage perso1 = new Personnage(); // creation object 
+            // Personnage perso2 = new Personnage();
+            perso1.seDeplacer();
+            perso1.attaquer();
+
+            perso1.Force = 50; // appelle au set 
+            Console.WriteLine("La force de perso1 est : " + perso1.Force);
+            perso1.Force += 25;
+            Console.WriteLine("La force de perso1 est : " + perso1.Force);
+
+            Console.WriteLine(perso1.obtenirDescription());
+
+            string msgDescription = perso1.obtenirDescription();
+            Console.WriteLine(msgDescription);
+
+
         }
     }
 }
